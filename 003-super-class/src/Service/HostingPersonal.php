3385c732cf49ea2abf0cb6ed17c969dbb2966812
Alexandre Espinosa Menor <aemenor@gmail.com>
2021-03-07 11:03:06 -0800

    posta ao dia

--- 003-super-class/src/Service/HostingPersonal.php
+++ 003-super-class/src/Service/HostingPersonal.php
@@ -24,12 +24,17 @@ class HostingPersonal extends Hosting {
      * Extends create and control if we need add PHP-fpm to hosting
      * @return bool
      */
-    public function create()
+    public function create(string $nameservice)
     {
-        if(parent::create()) {
+        if(parent::create($nameservice)) {
             if($this->with_php) {
                 parent::addPhp();
             }
         }
+    }
+
+    public function delete(string $nameservice, bool $backup = false)
+    {
+        // TODO: Implement delete() method.
     }
 }
